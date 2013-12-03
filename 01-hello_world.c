d0e53199d4bd9dd950af792b6ca5ea0bd1c13e0b
Pablo Caro <me@pcaro.es>
2013-12-03 18:29:50 +0100

    Header added. Reformat.

--- 01-hello_world.c
+++ 01-hello_world.c
@@ -1,28 +1,34 @@
+// ---------------------------------------------------
+// Copyright (c) 2013 Pablo Caro. All Rights Reserved.
+// Pablo Caro <me@pcaro.es> - https://pcaro.es/
+// 01-hello_world.c
+// ---------------------------------------------------
+
 #include <stdio.h>
 #include <mpi.h>
 
 int main(int argc, char **argv)
 {
 
-  // Initialize MPI
-  MPI_Init(&argc, &argv);
+    // Initialize MPI
+    MPI_Init(&argc, &argv);
 
-  // Establishes the size of the group
-  int nproc;
-  MPI_Comm_size(MPI_COMM_WORLD, &nproc);
+    // Establishes the size of the group
+    int nproc;
+    MPI_Comm_size(MPI_COMM_WORLD, &nproc);
 
-  // Establishes the rank of the calling process
-  int rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    // Establishes the rank of the calling process
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  // Gets the name of the processor
-  int processor_name_len;
-  char processor_name[MPI_MAX_PROCESSOR_NAME];
-  MPI_Get_processor_name(processor_name, &processor_name_len);
+    // Gets the name of the processor
+    int processor_name_len;
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    MPI_Get_processor_name(processor_name, &processor_name_len);
 
-  printf("Process: %2d   Processor: %s\n", rank, processor_name);
+    printf("Process: %2d   Processor: %s\n", rank, processor_name);
 
-  MPI_Finalize();
+    MPI_Finalize();
 
-  return 0;
+    return 0;
 }
